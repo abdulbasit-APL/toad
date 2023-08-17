db9781d903a9b2e55d4ff28ed63abd4a5bd986ec
abdulbasit-APL <abdulbasit@tenx.ai>
2023-08-17 21:25:08 +0500

    Testing

---
+++ ExecuteAllScripts.ps1
@@ -0,0 +1,8 @@
+# Get all .ps1 files in the Scripts folder
+$scriptFiles = Get-ChildItem -Path $(System.DefaultWorkingDirectory)\Scripts -Filter *.ps1
+
+# Loop through each script and execute it
+foreach ($scriptFile in $scriptFiles) {
+    Write-Host "Executing script $($scriptFile.Name)"
+    Invoke-Expression -Command ".\$($scriptFile.Name)"
+}
