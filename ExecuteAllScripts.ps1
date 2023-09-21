8742b1eaa3239fa0419b083591392554c87186e4
abdulbasit-APL <113006542+abdulbasit-APL@users.noreply.github.com>
2023-09-21 17:49:14 +0500

    Update ExecuteAllScripts.ps1

--- ExecuteAllScripts.ps1
+++ ExecuteAllScripts.ps1
@@ -1,6 +1,6 @@
 # Get all .ps1 files in the folder
 $scriptFiles = Get-ChildItem -Path $scriptsFolderPath -Filter *.ps1
-# Loop through each script and execute using the Toad DevOps Toolkit
+# Loop through each script and execute using the Toad DevOps Toolkit test123
             
 # Loop through each script and execute it using Toad
 foreach ($scriptFile in $scriptFiles) {
